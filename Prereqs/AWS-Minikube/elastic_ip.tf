c60bfd9acd461359bdd3763e2c351f3bfe9fa71e
Chris Adkin <chris@exadat.co.uk>
2023-03-17 08:54:37 +0000

    Additional outputs added to elastic_ip.tf

--- Prereqs/AWS-Minikube/elastic_ip.tf
+++ Prereqs/AWS-Minikube/elastic_ip.tf
@@ -5,9 +5,24 @@ resource "aws_eip" "linux-eip" {
   }
 }
 
+resource "aws_eip" "consul-dashboard-eip" {
+  vpc  = true
+  tags = {
+    Name = "consul-dashboard-eip"
+  }
+}
+
 resource "aws_eip_association" "linux-eip-association" {
   instance_id   = aws_instance.linux-server.id
   allocation_id = aws_eip.linux-eip.id
+}
+
+output "eip_dashboard_public_ip" {
+  value = "${aws_eip.consul-dashboard-eip.public_ip}"
+}
+
+output "eip_dashboard_allocation_id" {
+  value = "${aws_eip.consul-dashboard-eip.allocation_id}"
 }
 
 output "ssh_command" {
